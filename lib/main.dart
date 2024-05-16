4ec64c94d52c632ff5c2ff6e438acd39586c6ed4
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2024-05-16 23:00:15 +0530

    added basic flutter firebse auth

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:cash_cache/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'auth/auth_gate.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AuthGate(),
+    );
+  }
+}
